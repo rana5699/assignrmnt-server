f02ad23761ac94e317da3cc3c2829ad025307780
unknown <ranaot56@gmail.com>
2024-06-24 22:31:59 +0600

    add new port number

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-// assignmentserver56
-// 6Nqf2ULLwbBKeiVD
-
 app.listen(PORT, () => {
   console.log(`Server running at this port http://localhost:${PORT}`);
 });
